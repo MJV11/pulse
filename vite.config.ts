6bf20882e5d7cf90f5284251d45e742eb711a938
Max Vink <max.vink@berkeley.edu>
2026-05-01 15:42:16 -0700

    feat: routeable

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Serve index.html for all 404s so React Router handles client-side routes on refresh
-    historyApiFallback: true,
-  },
 })
